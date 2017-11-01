9b624e1fade3d00f587f09ae1dce06b79f84e80c
Joao Campos <j.campos4g@gmail.com>
2017-11-01 18:19:55 -0400

    fixed giant's behavior

--- src/Giant.java
+++ src/Giant.java
@@ -1,16 +1,35 @@
 import java.awt.*;
 
 public class Giant extends Critter {
+    private int count = 0;
 
     public Action getMove(CritterInfo info) {
-        return Action.LEFT;
+        if (info.getFront() == Neighbor.OTHER) {
+            return Action.INFECT;
+        } else if (info.getFront() == Neighbor.EMPTY) {
+            return Action.HOP;
+        } else {
+            return Action.RIGHT;
+        }
     }
 
     public Color getColor() {
-        return Color.BLUE;
+        return Color.GRAY;
     }
 
     public String toString() {
-        return "G";
+        if (count % 24 < 6) {
+            count++;
+            return "fee";
+        } else if (count % 24 < 12) {
+            count++;
+            return "fie";
+        } else if (count % 24 < 18) {
+            count++;
+            return "foe";
+        } else {
+            count++;
+            return "fum";
+        }
     }
 }
