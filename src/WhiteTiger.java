a8d53b2bc49e10265679d9f06e74ad0713c9a548
Joao Campos <j.campos4g@gmail.com>
2017-11-01 18:13:24 -0400

    fixed WhiteTiger's behavior

--- src/WhiteTiger.java
+++ src/WhiteTiger.java
@@ -1,2 +1,30 @@
+import java.awt.*;
+
 public class WhiteTiger extends Critter {
+    private boolean hasInfected = false;
+
+    public Action getMove(CritterInfo info) {
+        if (info.getFront() == Neighbor.OTHER) {
+            hasInfected = true;
+            return Action.INFECT;
+        } else if(info.getFront() == Neighbor.WALL || info.getRight() == Neighbor.WALL) {
+            return Action.LEFT;
+        } else if (info.getFront() == Neighbor.SAME) {
+            return Action.RIGHT;
+        } else {
+            return Action.HOP;
+        }
+    }
+
+    public Color getColor() {
+        return Color.WHITE;
+    }
+
+    public String toString() {
+        if (!hasInfected) {
+            return "tgr";
+        } else {
+            return "TGR";
+        }
+    }
 }
