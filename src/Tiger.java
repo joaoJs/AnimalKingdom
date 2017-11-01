17ec44058f994ddb57758fc7240c037dbd504f40
Joao Campos <j.campos4g@gmail.com>
2017-11-01 17:44:52 -0400

    fixed tiger's behavior

--- src/Tiger.java
+++ src/Tiger.java
@@ -1,16 +1,38 @@
 import java.awt.*;
 
 public class Tiger extends Critter {
+    private int count = 0;
+    private int random = (int)(Math.floor(Math.random() * 3));
 
     public Action getMove(CritterInfo info) {
-        return Action.HOP;
+        if (info.getFront() == Neighbor.OTHER) {
+            return Action.INFECT;
+        } else if(info.getFront() == Neighbor.WALL || info.getRight() == Neighbor.WALL) {
+            return Action.LEFT;
+        } else if (info.getFront() == Neighbor.SAME) {
+            return Action.RIGHT;
+        } else {
+            return Action.HOP;
+        }
     }
 
     public Color getColor() {
-        return Color.ORANGE;
+        if (count % 3 == 0) {
+            random = (int) (Math.floor(Math.random() * 3));
+        }
+        if (random == 1) {
+            count++;
+            return Color.RED;
+        } else if (random == 2) {
+            count++;
+            return Color.GREEN;
+        } else {
+            count++;
+            return Color.BLUE;
+        }
     }
 
     public String toString() {
-        return "T";
+        return "TGR";
     }
 }
