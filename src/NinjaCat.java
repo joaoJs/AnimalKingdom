0b5e6d0fbcf1c559eaaefc02d0829d4d263e2edc
Joao Campos <j.campos4g@gmail.com>
2017-11-01 18:36:27 -0400

    fixed ninja cat's behavior

--- src/NinjaCat.java
+++ src/NinjaCat.java
@@ -1,9 +1,23 @@
 import java.awt.*;
 
 public class NinjaCat extends Critter {
+    private int count = 0;
+    private int countSt = -1;
 
     public Action getMove(CritterInfo info) {
-        return Action.HOP;
+        if (info.getFront() == Neighbor.OTHER) {
+            return Action.INFECT;
+        } else if (info.getFront() == Neighbor.EMPTY) {
+            return Action.HOP;
+        } else {
+            if (count % 6 == 0) {
+                count++;
+                return Action.RIGHT;
+            } else {
+                count++;
+                return Action.LEFT;
+            }
+        }
     }
 
     public Color getColor() {
@@ -11,6 +25,7 @@ public class NinjaCat extends Critter {
     }
 
     public String toString() {
-        return "NC";
+        countSt++;
+        return "-" + countSt + "-";
     }
 }
